d7118f4756f098f27676a8f076cde25aeffd7058
HIROAKI-KAJI <kaji.tolearnprogramming@gmail.com>
2023-05-18 21:02:25 +0900

    update S001

--- S001/flet_apppage.py
+++ S001/flet_apppage.py
@@ -1,5 +1,33 @@
 import flet as ft
+from typing import TypeVar, Generic, Union, Callable
 
+T = TypeVar('T')
+
+#状態管理クラス。bind()で状態変更時に呼び出したい処理を登録できる。
+class State(Generic[T]):
+    def __init__(self, value: T):
+        self._value = value
+        self._observers: list[Callable] = []
+
+    def get(self):
+        return self._value #値の参照はここから
+
+    def set(self, new_value: T):
+        if self._value != new_value:
+            self._value = new_value #新しい値をセット
+            for observer in self._observers: observer() #変更時に各observerに通知する
+
+    def bind(self, observer):
+        self._observers.append(observer)# 変更時に呼び出す為のリストに登録
+
+
+text = State('')
+text.set('12345')
+print(text.get()) # -> 12345
+
+def on_change_handler():
+    print(text.get())
+text.bind(on_change_handler)
 
 class GreeterControl(ft.UserControl):
     def build(self):
@@ -7,6 +35,7 @@ class GreeterControl(ft.UserControl):
             content= ft.Row(
             controls = [
             ft.Text(value="Hello"),
+            ft.TextField(on_change=lambda e: text.set(e.control.value)),
             ]
             )
         )
