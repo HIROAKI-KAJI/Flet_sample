794a659519ee0afdbc48373858ec26ddfdeeb5d0
HIROAKI-KAJI <kaji.tolearnprogramming@gmail.com>
2023-05-26 23:01:05 +0900

    update S005

--- S005/flet_intarapt_view.py
+++ S005/flet_intarapt_view.py
@@ -1,2 +1,13 @@
+from typing import Any, List, Optional, Union
 import flet as ft
+from flet_core.control import Control, OptionalNumber
+from flet_core.ref import Ref
+from flet_core.types import AnimationValue, ClipBehavior, OffsetValue, ResponsiveNumber, RotateValue, ScaleValue
 
+class reaction_control(ft.UserControl):
+
+    def __init__(self, controls: List[Control] | None = None, ref: Ref | None = None, key: str | None = None, width: OptionalNumber = None, height: OptionalNumber = None, left: OptionalNumber = None, top: OptionalNumber = None, right: OptionalNumber = None, bottom: OptionalNumber = None, expand: bool | int | None = None, col: ResponsiveNumber | None = None, opacity: OptionalNumber = None, rotate: RotateValue = None, scale: ScaleValue = None, offset: OffsetValue = None, aspect_ratio: OptionalNumber = None, animate_opacity: AnimationValue = None, animate_size: AnimationValue = None, animate_position: AnimationValue = None, animate_rotation: AnimationValue = None, animate_scale: AnimationValue = None, animate_offset: AnimationValue = None, on_animation_end=None, visible: bool | None = None, disabled: bool | None = None, data: Any = None, clip_behavior: ClipBehavior | None = None):
+        super().__init__(controls, ref, key, width, height, left, top, right, bottom, expand, col, opacity, rotate, scale, offset, aspect_ratio, animate_opacity, animate_size, animate_position, animate_rotation, animate_scale, animate_offset, on_animation_end, visible, disabled, data, clip_behavior)
+
+    def build(self):
+        return super().build()
